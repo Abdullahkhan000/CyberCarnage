45ab69e832b4ea1f3bcc375e336c825c1797dcdb
TexasRattleSnake007 <101890335+Abdullahkhan000@users.noreply.github.com>
2025-12-05 01:19:26 +0500

    url changing v2

--- data/urls.py
+++ data/urls.py
@@ -12,21 +12,6 @@ urlpatterns = [
     path("about_game/", views.AboutView.as_view()),
     path("about_game/<int:pk>/", views.AboutView.as_view()),
 
-    # path("cybercarnage/", views.games_list_view, name="home"),
-    # path("cybercarnage/about/", views.about_list_view, name="about_list"),
-    # path("cybercarnage/game_list/", views.gameinfo_list_view, name="gameinfo_list"),
-    #
-    # path("about/<int:pk>/", views.about_detail_view, name="about_detail"),
-    #
-    # path("contact/send/", views.send_contact, name="send_contact"),
-    #
-    # path("subscribe/", views.subscribe_newsletter, name="subscribe_newsletter"),
-    #
-    # path("cybercarnage/privacy/", views.privacy_page, name="privacy"),
-    # path("cybercarnage/terms/", views.terms_page, name="terms"),
-    # path("cybercarnage/faq/", views.faq_page, name="faq"),
-
-
     # ---------------- NEW CLEAN ROOT URLS ----------------
     path("", views.games_list_view, name="new_home"),
     path("about/", views.about_list_view, name="new_about_list"),
