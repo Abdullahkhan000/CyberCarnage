aec660934971eff060d87665f5741aa2741d83b3
TexasRattleSnake007 <101890335+Abdullahkhan000@users.noreply.github.com>
2026-01-19 15:50:38 +0500

    Add Discord verification v2

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,25 @@
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("", include("data.urls")),
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("data.urls")),
+
+    # Discord HTTPS verification
+    path('.well-known/discord', serve, {
+        'path': 'discord',
+        'document_root': os.path.join(BASE_DIR, '.well-known')
+    }),
 ]
